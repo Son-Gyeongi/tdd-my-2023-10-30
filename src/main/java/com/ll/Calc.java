e575fc8ec1fd039d656a2dcaf1e127c26beb6c57
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-30 18:49:25 +0900

    혼자서 테스트 t2~5 더하기

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,6 +2,13 @@ package com.ll;
 
 public class Calc {
     public static int run(String exp) { // exp, expression 표현식
-        return 20;
+        // 값이 없을 경우
+        if (exp.isBlank()) return 0;
+
+        String[] expBits = exp.split(" \\+ ");
+        int num1 = Integer.parseInt(expBits[0]);
+        int num2 = Integer.parseInt(expBits[1]);
+
+        return num1 + num2;
     }
 }
